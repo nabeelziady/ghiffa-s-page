952adb9f9fd4b0c964155117741490b66b293dfb
nabeelziady <nabeelziady31@gmail.com>
2025-07-25 16:37:51 +0700

    Update 3_The_Day_of_Confession.py

--- pages/3_The_Day_of_Confession.py
+++ pages/3_The_Day_of_Confession.py
@@ -13,8 +13,8 @@ with col2:
 with col3:
     st.image("gipa22.jpg", use_container_width=True)
     
-st.title("The Day I Confessed 𝖂𝖍𝖆𝖙 𝖙𝖍𝖊 𝖘𝖎𝖌𝖒𝖆")
-st.subheader("It Was Such a Pleasure !")
+st.title("The Day I Confessed ℐ𝓁ℴ𝓋ℯ 𝓎ℴ𝓊 <3")
+st.subheader("It Was Such a Pleasure !❤️")
 st.write("21th of April 2025, the day the day the daayyyyyy Nabeel got the honor to date such a precious, wonderful, beautiful, unimagineable, and a fairytale lady that Nabeel had always looking for. It was such a pleasure to be a boyfriend of such a fairy. Even till today, Nabeel still grateful that he can be hers.")
 
 st.write("It was when we went to play in Bandung, we went to several places like cafe, Disclosure, Braga, and a dinner at Yamcha Braga. We did three photobooths that time even though we're not good at posing (well she looks so fine in anyway). And when we went back to Jatinangor, Nabeel decided to take the shot at that night. He ask her if they want to stay a few time more at Student Center Unpad, so he brought his laptop first from his kosan and he showed up the python codes that he made for confession. It was such a really nervousing situation, but luckily there was someone that played some music that support the vibes. No words can describe how happy Nabeel was at that time, it was like a dream. He will never ever forget about that time, that day, and that date.")
