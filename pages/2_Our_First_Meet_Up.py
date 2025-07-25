77c228cb4e0bc69acfcf4f244636d81069743ac1
nabeelziady <nabeelziady31@gmail.com>
2025-07-25 16:05:37 +0700

    Update 2_Our_First_Meet_Up.py

--- pages/2_Our_First_Meet_Up.py
+++ pages/2_Our_First_Meet_Up.py
@@ -15,6 +15,8 @@ with col3:
     
 st.title("The First Time We Met 𝖂𝖍𝖆𝖙 𝖙𝖍𝖊 𝖘𝖎𝖌𝖒𝖆")
 st.subheader("One of the BEST DAY Nabeel Ever Had !")
-st.write("April 10th 2025, 'iuwhaihufegqfgiagf' is what nabeel feel about that day, it was just too cute twin🥀, but it's so real though, Nabeel really prepared EVERYTHING the day before, like an umbrella, 2 raincoats, first aid kit, tissues, perfumes, and others. He also made a whole rundown for the day, even though it kinda delayed because of the rain and traffic, but it was all wrapped.")
+st.write("April 10th 2025, 'iuwhaihufegqfgiagf' is what nabeel feel about that day, it was just too cute twin🥀, but it's so real though, Nabeel really prepared EVERYTHING the day before, like an umbrella, 2 raincoats, first aid kit, tissues, perfumes, and others. He also made a whole rundown for the day, even though it was kinda delayed because of the rain and traffic, but it was all wrapped.")
 
 st.write("What's funny is, Nabeel made a chit-chat scenario in his head when he meet her, but when it comes, his head was BLANKED and all he did was an awkward laugh and some nervousness. Well he was too stunned by her beauty bro it can't be helped😹✌️, but you know she smells ssooooooooooooooooooo goooooooood that time, her smiles also makes him relieved from all the upset because of the rain.")
+
+st.write("Nabeel really had a super fun time that day, and enthusiasm from her really really really makes him happy, we played bowling, arcades, photobooth, took a dinner at Kimukatsu, ate a matcha ice cream, and did some tiktoks especially 'Last Train at 25 O'Clock' that Nabeel has been waiting for all this time!, Nabeel still remembers the smiles that he had after the hangout with her.")
