f873669be4a1a349222a53fdcdd9ed787c83e48a
nabeelziady <nabeelziady31@gmail.com>
2025-07-25 15:12:45 +0700

    Update 1_How_It_Started.py

--- pages/1_How_It_Started.py
+++ pages/1_How_It_Started.py
@@ -11,8 +11,8 @@ with col1:
 with col2:
     st.image("gipa15.jpg", use_container_width=300)
     
-st.title("The First Time We Know Each Other")
-st.subheader("DMs All The Way")
+st.title("The First Time We Know Each Other ⋆𐙚₊")
+st.subheader("DMs All The Way ‪‪❤︎‬")
 st.write("It was the 12th of March 2025, when Nabeel started his shot with 'lucu kak gambarnya' at 10 pm." \
 " It is the only DM that nabeel will never regret because it led him to such a crazy bag. He was really nervous at first, but ghiffa's replies was so adorable it made nabeel sooooo happy." \
 " Nabeel's first impression was he didn't expect that she was this funny and her jokes matches with him." \
