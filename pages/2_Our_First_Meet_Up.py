68b3db28cf64324b384d17f5f92e10644685687e
nabeelziady <nabeelziady31@gmail.com>
2025-07-25 15:04:34 +0700

    Update 2_Our_First_Meet_Up.py

---
+++ pages/2_Our_First_Meet_Up.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="How It Started | Home",
+    layout="centered"
+)
+
+_, center_col, _ = st.columns([1, 2, 1])
+with center_col:
+    st.image("gipa15.jpg", width=300)
+st.title("The First Time We Know Each Other")
+st.subheader("DMs All The Way")
+st.write("
