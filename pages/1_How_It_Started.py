10654cc33299faadd99bdf76a2fd9a402f12a79b
nabeelziady <nabeelziady31@gmail.com>
2025-07-25 15:26:56 +0700

    Update 1_How_It_Started.py

--- pages/1_How_It_Started.py
+++ pages/1_How_It_Started.py
@@ -17,3 +17,5 @@ st.write("It was the 12th of March 2025, when Nabeel started his shot with 'lucu
 " It is the only DM that nabeel will never regret because it led him to such a crazy bag. He was really nervous at first, but ghiffa's replies was so adorable it made nabeel sooooo happy." \
 " Nabeel's first impression was he didn't expect that she was this funny and her jokes matches with him." \
 " Until today, Nabeel still remembers the day that he will never ever regret in his life, the day that turned everything differently.")
+
+st.write("We talked a lot of things that time, from A to Z that we've talked about and it actually so surprising that we like the same things from food, entertainment, jokes, games, and many more. Nabeel felt like finally he has found the one that he's been waiting for all this time, and that makes him REALLY didn't want to fumble this precious one.'
