d6a9843d5a2530eba7a4c28b3dbd245210d0be58
nabeelziady <nabeelziady31@gmail.com>
2025-07-25 15:10:00 +0700

    Update 1_How_It_Started.py

--- pages/1_How_It_Started.py
+++ pages/1_How_It_Started.py
@@ -7,7 +7,7 @@ st.set_page_config(
 
 col1, col2 = st.columns(2)
 with col1:
-    st.image("gipa16.jpg", use_container_width=true)
+    st.image("gipa16.jpg", use_container_width=True)
 with col2:
     st.image("gipa15.jpg", use_container_width=300)
     
