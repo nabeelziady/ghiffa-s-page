58e4145db9c218d1e0c09726d1fc731cf1540c67
nabeelziady <nabeelziady31@gmail.com>
2025-07-25 15:29:08 +0700

    Update 1_How_It_Started.py

--- pages/1_How_It_Started.py
+++ pages/1_How_It_Started.py
@@ -12,10 +12,10 @@ with col2:
     st.image("gipa15.jpg", use_container_width=300)
     
 st.title("The First Time We Know Each Other⋆𐙚₊")
-st.subheader("DMs All The Way ‪!‬")
-st.write("It was the 12th of March 2025, when Nabeel started his shot with 'lucu kak gambarnya' at 10 pm." \
+st.subheader("DMs All The Way ‪!‬ 🎀")
+st.write("    It was the 12th of March 2025, when Nabeel started his shot with 'lucu kak gambarnya' at 10 pm." \
 " It is the only DM that nabeel will never regret because it led him to such a crazy bag. He was really nervous at first, but ghiffa's replies was so adorable it made nabeel sooooo happy." \
 " Nabeel's first impression was he didn't expect that she was this funny and her jokes matches with him." \
 " Until today, Nabeel still remembers the day that he will never ever regret in his life, the day that turned everything differently.")
 
-st.write("We talked a lot of things that time, from A to Z that we've talked about and it actually so surprising that we like the same things from food, entertainment, jokes, games, and many more. Nabeel felt like finally he has found the one that he's been waiting for all this time, and that makes him REALLY didn't want to fumble this precious one.")
+st.write("    We talked a lot of things that time, from A to Z that we've talked about and it actually so surprising that we like the same things from food, entertainment, jokes, games, and many more. Nabeel felt like finally he has found the one that he's been waiting for all this time, and that makes him REALLY didn't want to fumble this precious one.")
