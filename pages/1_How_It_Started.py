08d2b8a2b60385b2c15c8937273c2525d227b36c
nabeelziady <nabeelziady31@gmail.com>
2025-07-25 15:04:12 +0700

    Update 1_How_It_Started.py

--- pages/1_How_It_Started.py
+++ pages/1_How_It_Started.py
@@ -5,11 +5,12 @@ st.set_page_config(
     layout="centered"
 )
 
-st.image("gipa15.jpg",
-        use_container_width=True)
+_, center_col, _ = st.columns([1, 2, 1])
+with center_col:
+    st.image("gipa15.jpg", width=300)
 st.title("The First Time We Know Each Other")
 st.subheader("DMs All The Way")
 st.write("It was the 12th of March 2025, when Nabeel started his shot with 'lucu kak gambarnya' at 10 pm." \
-"It is the only DM that nabeel will never regret because it led him to such a crazy bag. He was really nervous at first, but ghiffa's replies was so adorable it made nabeel sooooo happy." \
-"Nabeel's first impression was he didn't expect that she was this funny and her jokes matches with him." \
-"Until today, Nabeel still remembers the day that he will never ever regret in his life, the day that turned everything becomes beautiful.")
+" It is the only DM that nabeel will never regret because it led him to such a crazy bag. He was really nervous at first, but ghiffa's replies was so adorable it made nabeel sooooo happy." \
+" Nabeel's first impression was he didn't expect that she was this funny and her jokes matches with him." \
+" Until today, Nabeel still remembers the day that he will never ever regret in his life, the day that turned everything differently.")
